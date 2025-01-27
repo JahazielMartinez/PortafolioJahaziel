a285ba5892627fa8404f436c0b75c2619bbe7d84
Jahaziel Martinez <jahazielm3@gmail.com>
2025-01-27 16:25:39 -0600

    translate.js update

--- pages/api/translate.js
+++ pages/api/translate.js
@@ -1,18 +1,13 @@
-import translate from "../../util/translateCliente"; // Ruta relativa al archivo translateClient.js
+const translate = require("../../util/translateCliente");
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { text, targetLanguage } = req.body;
+  const { text, targetLanguage } = req.body;
 
-    try {
-      const [translation] = await translate.translate(text, targetLanguage);
-      res.status(200).json({ translatedText: translation });
-    } catch (error) {
-      console.error("Translation error:", error);
-      res.status(500).json({ error: "Translation failed", details: error.message });
-    }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  try {
+    const [translation] = await translate.translate(text, targetLanguage);
+    res.status(200).json({ translation });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error translating text" });
   }
 }
